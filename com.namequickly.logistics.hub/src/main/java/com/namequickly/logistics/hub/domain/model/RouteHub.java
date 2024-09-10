42c96ee47329c6b6e7f4a59b6002926e181c1340
jin66 <dbf6609@naver.com>
2024-09-11 02:55:44 +0900

    feat: gateway, auth, hub 폴더구조 및 yml 설정 (1차)

---
+++ com.namequickly.logistics.hub/src/main/java/com/namequickly/logistics/hub/domain/model/RouteHub.java
@@ -0,0 +1,54 @@
+package com.namequickly.logistics.hub.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "p_route_hub")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RouteHub extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID routeHubId;
+
+    private UUID routeId;
+    private UUID hubId;
+    private Integer orderInRoute;
+    private Double distanceToNextHub;
+    private Double timeToNextHub;
+    private Boolean isDelete;
+
+    public static RouteHub createRouteHub(RouteHubRequestDto routeHubDTO) {
+        return RouteHub.builder()
+            .routeId(routeHubDTO.getRouteId())
+            .hubId(routeHubDTO.getHubId())
+            .orderInRoute(routeHubDTO.getOrderInRoute())
+            .distanceToNextHub(routeHubDTO.getDistanceToNextHub())
+            .timeToNextHub(routeHubDTO.getTimeToNextHub())
+            .isDelete(routeHubDTO.getIsDelete())
+            .build();
+    }
+
+    public void updateRouteHub(RouteHubRequestDto routeHubDTO) {
+        this.routeId = routeHubDTO.getRouteId();
+        this.hubId = routeHubDTO.getHubId();
+        this.orderInRoute = routeHubDTO.getOrderInRoute();
+        this.distanceToNextHub = routeHubDTO.getDistanceToNextHub();
+        this.timeToNextHub = routeHubDTO.getTimeToNextHub();
+        this.isDelete = routeHubDTO.getIsDelete();
+    }
+}
