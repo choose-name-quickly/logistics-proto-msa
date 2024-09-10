fca2c390c6b809f57512ab42a98d416ba6019b82
JianBBB <sbganne@naver.com>
2024-09-11 08:24:19 +0900

    config: Order, Product-Company 서비스, 개발 단계에서 시큐리티 비활성화 설정

--- com.namequickly.logistics.product-company/src/main/java/com/namequickly/logistics/product_company/ProductCompanyApplication.java
+++ com.namequickly.logistics.product-company/src/main/java/com/namequickly/logistics/product_company/ProductCompanyApplication.java
@@ -2,10 +2,11 @@ package com.namequickly.logistics.product_company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // 개발위해 시큐리티 임시 비활성화
 public class ProductCompanyApplication {
 
 	public static void main(String[] args) {
