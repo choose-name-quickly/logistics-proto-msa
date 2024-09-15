7dfc7f2c1f2006c7881701a07d21be3f10676fd0
JianBBB <sbganne@naver.com>
2024-09-15 23:12:40 +0900

    feat: order - order repository 파일 추가

---
+++ com.namequickly.logistics.order/src/main/java/com/namequickly/logistics/order/infrastructure/repository/OrderRepository.java
@@ -0,0 +1,32 @@
+package com.namequickly.logistics.order.infrastructure.repository;
+
+import com.namequickly.logistics.order.domain.model.order.Order;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+    @Query("SELECT o FROM Order o " +
+        "JOIN o.orderProducts op " +
+        "JOIN o.delivery d " +
+        "JOIN d.deliveryRoutes dr " +
+        "WHERE o.orderId = :orderId AND o.isDelete = :isDelete")
+    Optional<Order> findOrderDetails(@Param("orderId") UUID orderId,
+        @Param("isDelete") boolean isDelete);
+
+    @Query(value = "SELECT DISTINCT o FROM Order o " +
+        "JOIN o.orderProducts op " +
+        "JOIN o.delivery d " +
+        "JOIN d.deliveryRoutes dr " +
+        "WHERE o.isDelete = :isDelete",
+        countQuery = "SELECT COUNT(DISTINCT o.orderId) FROM Order o WHERE o.isDelete = :isDelete")
+    Page<Order> findAllOrderDetails(Pageable pageable, @Param("isDelete") boolean isDelete);
+}
+
