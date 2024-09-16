0886c5802f2cf258dd4264dc1f093a27571ada69
JianBBB <sbganne@naver.com>
2024-09-16 12:59:51 +0900

    feat: order  -  client 추가

---
+++ com.namequickly.logistics.order/src/main/java/com/namequickly/logistics/order/infrastructure/client/CompanyClient.java
@@ -0,0 +1,14 @@
+package com.namequickly.logistics.order.infrastructure.client;
+
+import java.util.UUID;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "hub-management-service")
+public interface CompanyClient {
+
+    // 해당 company 존재 여부 확인
+    @GetMapping("/api/hub-management/companies")
+    boolean checkCompanyExists(@RequestParam(name = "supplierId") UUID supplierId);
+}
