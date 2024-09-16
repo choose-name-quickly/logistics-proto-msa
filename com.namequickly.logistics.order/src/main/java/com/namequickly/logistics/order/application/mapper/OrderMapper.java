49597f2eedc38257612ee3b3613f137233947e61
JianBBB <sbganne@naver.com>
2024-09-16 12:56:35 +0900

    feat: order  - Mapper 파일 추가 및 변경

--- com.namequickly.logistics.order/src/main/java/com/namequickly/logistics/order/application/mapper/OrderMapper.java
+++ com.namequickly.logistics.order/src/main/java/com/namequickly/logistics/order/application/mapper/OrderMapper.java
@@ -1,6 +1,9 @@
 package com.namequickly.logistics.order.application.mapper;
 
+import com.namequickly.logistics.order.application.dto.OrderCreateResponseDto;
+import com.namequickly.logistics.order.application.dto.OrderDeleteResponseDto;
 import com.namequickly.logistics.order.application.dto.OrderResponseDto;
+import com.namequickly.logistics.order.application.dto.OrderUpdateResponseDto;
 import com.namequickly.logistics.order.domain.model.delivery.Delivery;
 import com.namequickly.logistics.order.domain.model.delivery.DeliveryRoute;
 import com.namequickly.logistics.order.domain.model.order.Order;
@@ -8,18 +11,26 @@ import com.namequickly.logistics.order.domain.model.order.OrderProduct;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface OrderMapper {
-
-    OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
 
     // Order에서 Delivery와 DeliveryRoutes를 참조해 자동으로 매핑
     @Mapping(source = "createdAt", target = "createdAt")
     @Mapping(source = "updatedAt", target = "updatedAt")
     @Mapping(source = "isDelete", target = "isDelete")
     OrderResponseDto toOrderResponseDto(Order order);
+
+    @Mapping(source = "order.orderId", target = "orderId")
+    @Mapping(source = "order.deletedAt", target = "deletedAt")
+    OrderDeleteResponseDto toOrderDeleteResponseDto(Order order);
+
+    @Mapping(source = "order.orderId", target = "orderId")
+    @Mapping(source = "order.updatedAt", target = "updatedBy")
+    @Mapping(source = "orderProduct.productId", target = "productId")
+    @Mapping(source = "orderProduct.orderQuantity", target = "orderQuantity")
+    OrderUpdateResponseDto toOrderUpdateResponseDto(Order order, OrderProduct orderProduct);
+
 
     List<OrderResponseDto.OrderProductDto> toOrderProductDtos(List<OrderProduct> orderProducts);
 
@@ -32,4 +43,5 @@ public interface OrderMapper {
     @Mapping(source = "isDelete", target = "isDelete")
     List<OrderResponseDto.DeliveryRouteDto> toDeliveryRouteDtos(List<DeliveryRoute> deliveryRoutes);
 
+    OrderCreateResponseDto toOrderCreateResponseDto(Order order);
 }
