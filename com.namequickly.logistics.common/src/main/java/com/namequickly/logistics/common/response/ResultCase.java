439458fb4b769daec15c2282d8f685bd1ef3b7b2
JianBBB <sbganne@naver.com>
2024-09-16 13:00:10 +0900

    feat: 공통 파일 - ResultCase 변경

--- com.namequickly.logistics.common/src/main/java/com/namequickly/logistics/common/response/ResultCase.java
+++ com.namequickly.logistics.common/src/main/java/com/namequickly/logistics/common/response/ResultCase.java
@@ -36,7 +36,45 @@ public enum ResultCase {
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, 2004, "만료된 Access Token"),
     // 만료된 리프레쉬 토큰 401
     EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, 2005, "만료된 Refresh Token"),
-    ;
+
+
+    /* 허브 3000번대 */
+    // 존재하지 않은 허브 404
+    // TODO 허브 개발 완료되면 다시 변경 예정
+    // 존재하지 않는 허브 404
+    NOT_FOUND_HUB(HttpStatus.NOT_FOUND, 3000, "허브를 찾을 수 없습니다."),
+    // 유효하지 않은 권한 403
+    UNAUTHORIZED_HUB(HttpStatus.FORBIDDEN, 3001, "작업을 수행할 수 있는 권한이 없습니다."),
+
+
+    /* 상품 관련 4000번대 */
+    // 존재하지 않는 상품 404
+    NOT_FOUND_PRODUCT(HttpStatus.NOT_FOUND, 4000, "상품을 찾을 수 없습니다."),
+    // 배송중인 상품 삭제 불가 400
+    CANNOT_DELETE_PRODUCT_IN_DELIVERY(HttpStatus.BAD_REQUEST, 4001, "배송 중인 상품은 삭제할 수 없습니다."),
+    // 중복된 상품명 입력 409
+    DUPLICATED_PRODUCT_NAME(HttpStatus.CONFLICT, 4002, "중복된 상품명을 입력하셨습니다."),
+    // 유효하지 않은 접근 400
+    UNAUTHORIZED_DELETE_PRODUCT(HttpStatus.BAD_REQUEST, 4003, "삭제된 상품에 접근할 수 없습니다."),
+
+    /* 회사 관련 5000번대 */
+    // TODO 회사 개발 완료되면 다시 변경 예정
+    // 존재하지 않는 업체 404
+    NOT_FOUND_COMPANY(HttpStatus.NOT_FOUND, 5000, "업체를 찾을 수 없습니다."),
+    // 유효하지 않은 권한 403
+    UNAUTHORIZED_COMPANY(HttpStatus.FORBIDDEN, 5001, "작업을 수행할 수 있는 권한이 없습니다."),
+
+    /* 배송 관련 6000번대 */
+    NOT_FOUND_DELIVERY(HttpStatus.NOT_FOUND, 3000, "배송 정보를 찾을 수 없습니다."),
+
+    /* 주문 관련 7000번대 */
+    // 존재하지 않는 업체 404
+    NOT_FOUND_ORDER(HttpStatus.NOT_FOUND, 7000, "주문을 찾을 수 없습니다."),
+    // 배송중인 주문 취소 불가 400
+    CANNOT_DELETE_ORDER_IN_DELIVERY(HttpStatus.BAD_REQUEST, 7001, "배송 중인 주문은 취소할 수 없습니다."),
+    // 배송중인 상품 수정 불가 400
+    CANNOT_UPDATE_ORDER_IN_DELIVERY(HttpStatus.BAD_REQUEST, 7001, "배송 중인 주문은 변경할 수 없습니다.");
+
 
     private final HttpStatus httpStatus; // 응답 상태 코드
     private final Integer code; // 응답 코드. 도메인에 따라 1000번대로 나뉨
