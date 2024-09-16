93c1a5214579cbd5622f041aaff02e22f4ae4b99
JianBBB <sbganne@naver.com>
2024-09-16 12:59:22 +0900

    feat: product  - mapper 변경

--- com.namequickly.logistics.product-company/src/main/java/com/namequickly/logistics/product_company/application/mapper/ProductMapper.java
+++ com.namequickly.logistics.product-company/src/main/java/com/namequickly/logistics/product_company/application/mapper/ProductMapper.java
@@ -10,13 +10,14 @@ import com.namequickly.logistics.product_company.domain.model.Product;
 import com.namequickly.logistics.product_company.domain.model.Stock;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     //ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
-    @Mapping(target = "productId", ignore = true)  // 업데이트 시 productId는 무시
+    @Mapping(target = "productId", ignore = true)
+        // 업데이트 시 productId는 무시
     Product toProduct(ProductUpdateRequestDto requestDto);
 
     @Mapping(source = "stockQuantity", target = "stockQuantity")
@@ -27,11 +28,14 @@ public interface ProductMapper {
     @Mapping(source = "product.updatedAt", target = "updatedAt")
     @Mapping(source = "product.isDelete", target = "isDelete")
     ProductGetResponseDto toProductGetResponseDto(Product product);
+
     @Mapping(source = "product.createdAt", target = "createdAt")
     ProductCreateResponseDto toProductCreateResponseDto(Product product);
+
     @Mapping(source = "product.updatedAt", target = "updatedAt")
     @Mapping(source = "product.stock.stockQuantity", target = "stockQuantity")
     ProductUpdateResponseDto toProductUpdateResponseDto(Product product);
+
     @Mapping(source = "product.deletedAt", target = "deletedAt")
     ProductDeleteResponseDto toProductDeleteResponseDto(Product product);
 }
