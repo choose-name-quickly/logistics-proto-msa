512b9ddbbc4e38b448eae021f09171bee092bae7
JianBBB <sbganne@naver.com>
2024-09-18 08:04:02 +0900

    fix: product_company - controller 변경

--- com.namequickly.logistics.product-company/src/main/java/com/namequickly/logistics/product_company/presentation/controller/ProductController.java
+++ com.namequickly.logistics.product-company/src/main/java/com/namequickly/logistics/product_company/presentation/controller/ProductController.java
@@ -16,7 +16,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -99,7 +98,7 @@ public class ProductController {
     }
 
     // feign client를 위한 api
-    @PatchMapping("/api/product-companies/{product_id}/stocks")
+    @PutMapping("/product-companies/{product_id}/stocks")
     public Integer updateStockQuantity(@PathVariable("product_id") UUID productId,
         @RequestBody StockUpdateRequest stockUpdateRequest) {
         return productService.updateStockQuantity(productId, stockUpdateRequest);
