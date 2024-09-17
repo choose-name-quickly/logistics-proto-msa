63c96ea2eeee87434c445f1ec81886711a7fff73
JianBBB <sbganne@naver.com>
2024-09-18 07:59:50 +0900

    fix: order - mapper 변경

--- com.namequickly.logistics.order/src/main/java/com/namequickly/logistics/order/application/mapper/DeliveryMapper.java
+++ com.namequickly.logistics.order/src/main/java/com/namequickly/logistics/order/application/mapper/DeliveryMapper.java
@@ -19,7 +19,6 @@ public interface DeliveryMapper {
     @Mapping(source = "delivery.recipientSlackId", target = "recipientSlackID")
     @Mapping(source = "delivery.deliveryStatus", target = "deliveryStatus")
     @Mapping(source = "delivery.createdAt", target = "createdAt")
-    @Mapping(source = "delivery.updatedAt", target = "updatedAt")
     @Mapping(source = "delivery.deliveryRoutes", target = "deliveryRoutes")
     DeliveryGetResponseDto toDeliveryGetResponseDto(Delivery delivery);
 
@@ -27,11 +26,7 @@ public interface DeliveryMapper {
     @Mapping(source = "deliveryRoute.deliveryRouteId", target = "deliveryRouteId")
     @Mapping(source = "deliveryRoute.routeHubId", target = "routeHubId")
     @Mapping(source = "deliveryRoute.courierId", target = "courierId")
-    @Mapping(source = "deliveryRoute.actualDistance", target = "actualDistance")
-    @Mapping(source = "deliveryRoute.actualDuration", target = "actualTime")
     @Mapping(source = "deliveryRoute.deliveryStatus", target = "deliveryStatus")
-    @Mapping(source = "deliveryRoute.createdAt", target = "createdAt")
-    @Mapping(source = "deliveryRoute.updatedAt", target = "updatedAt")
     DeliveryRouteDto toDeliveryRouteDto(DeliveryRoute deliveryRoute);
 
     // List<DeliveryRoute> -> List<DeliveryRouteDto> 매핑
