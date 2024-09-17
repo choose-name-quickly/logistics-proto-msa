2ad72f76e5d86658853a3572fd00cb239cb5cd1c
uzuberceuse <cerulesky@naver.com>
2024-09-17 12:51:25 +0900

    ai controller 추가

---
+++ com.namequickly.logistics.ai/src/main/java/com/namequickly/logistics/ai/presentation/controller/AIController.java
@@ -0,0 +1,23 @@
+package com.namequickly.logistics.ai.presentation.controller;
+
+import com.namequickly.logistics.ai.application.other.GeminiService;
+import com.namequickly.logistics.ai.application.other.WeatherService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/ai")
+public class AIController {
+
+
+
+
+    // 날씨 정보 요청
+
+    public void weatherInit(){
+
+
+    }
+}
