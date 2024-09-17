e76ce15b9522be56032dbeb13f633acc97045752
JianBBB <sbganne@naver.com>
2024-09-18 08:01:12 +0900

    fix: order - Client 변경

--- com.namequickly.logistics.order/src/main/java/com/namequickly/logistics/order/infrastructure/client/HubClient.java
+++ com.namequickly.logistics.order/src/main/java/com/namequickly/logistics/order/infrastructure/client/HubClient.java
@@ -1,8 +1,9 @@
 package com.namequickly.logistics.order.infrastructure.client;
 
-import com.namequickly.logistics.order.application.dto.client.HubDto;
+import com.namequickly.logistics.common.response.CommonResponse;
+import com.namequickly.logistics.order.application.dto.client.HubResponseDto;
 import com.namequickly.logistics.order.application.dto.client.HubRouteCourierDto;
-import com.namequickly.logistics.order.application.dto.client.HubRouteDto;
+import com.namequickly.logistics.order.application.dto.client.RouteHubResponseDto;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -21,13 +22,13 @@ public interface HubClient {
         @RequestParam(name = "destinationHubId") UUID destinationHubId);
 
     // TODO 허브에게 routeHubId(=startHubId) 로 허브 정보 가져와야함
-    @GetMapping("/api/hubs/{hub_id}")
-    HubDto getHub(@PathVariable("hub_id") UUID hubId);
+    @GetMapping("/api/hubs/{hubId}")
+    CommonResponse<HubResponseDto> getHub(@PathVariable("hubId") UUID hubId);
 
-    @GetMapping("/api/hub-routes/{route_hub_id}")
-    HubRouteDto getHubRoute(@PathVariable("route_hub_id") UUID routeHubId);
+    @GetMapping("/api/route-hubs/{routeHubId}")
+    CommonResponse<RouteHubResponseDto> getRouteHub(@PathVariable("routeHubId") UUID routeHubId);
 
-    @GetMapping("/api/routes/{route_id}")
-    List<HubRouteDto> getHubRoutes(@PathVariable("route_id") UUID routeId);
-
+    /*@GetMapping("/api/routes/{routeId}")
+    List<RouteHubResponseDto> getRoute(@PathVariable("routeId") UUID routeId);
+*/
 }
