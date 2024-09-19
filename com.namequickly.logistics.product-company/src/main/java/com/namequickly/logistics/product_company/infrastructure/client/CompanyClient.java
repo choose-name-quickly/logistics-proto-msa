a8a1852bffe7f626464431d53dd351ff80a48751
JianBBB <sbganne@naver.com>
2024-09-19 20:30:39 +0900

    fix: order, product-company - company client 수정

--- com.namequickly.logistics.product-company/src/main/java/com/namequickly/logistics/product_company/infrastructure/client/CompanyClient.java
+++ com.namequickly.logistics.product-company/src/main/java/com/namequickly/logistics/product_company/infrastructure/client/CompanyClient.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "hub-management-service")
 public interface CompanyClient {
@@ -18,4 +19,7 @@ public interface CompanyClient {
     CompanyResponse getCompanyById(@PathVariable UUID companyId,
         @RequestHeader(value = "X-User-Role") String role,
         @RequestHeader(value = "X-User-AffiliationId") UUID affiliationId);
+
+    @GetMapping("/api/companies/checkId")
+    boolean checkId(@RequestParam UUID companyId);
 }
